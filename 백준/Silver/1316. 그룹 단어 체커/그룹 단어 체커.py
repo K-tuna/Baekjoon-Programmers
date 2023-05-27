ac05fab25fe2ec38562f768464beb99a91048830
dongwonkwon <90139277+dongwonkwon@users.noreply.github.com>
2023-05-28 02:22:51 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -2,10 +2,10 @@ n=int(input()) #단어의 개수 입력
 result=0
 for _ in range(n):
     word=['']+list(input()) #단어를 리스트로 입력받음 예)['','h','a','p','p','y']
-    memory=[]
+    memory=set()
     for i in range(1,len(word)):
         if word[i] in memory and word[i]!=word[i-1]:
             result+=1
             break
-        memory.append(word[i])
+        memory.add(word[i])
 print(n-result)
