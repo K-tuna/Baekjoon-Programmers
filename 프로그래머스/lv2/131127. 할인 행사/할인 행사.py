d56e133368af09c30bde611835dffd8d9ba2b6ab
dongwonkwon <90139277+dongwonkwon@users.noreply.github.com>
2023-06-14 01:20:06 +0900

    [level 2] Title: 할인 행사, Time: 0.02 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.py
@@ -1,13 +1,11 @@
-import numpy
+from collections import Counter
 def solution(want, number, discount):
     cnt=0
-    lw=len(want)
+    dic={}
+    for i in range(len(want)):
+        dic[want[i]]=number[i]
+    
     for i in range(len(discount)-9):
-        part=discount[i:i+10]
-        dc=[0]*lw
-        for j in part:
-            if j in want:
-                dc[want.index(j)]+=1
-        if sum(numpy.array(dc)>=numpy.array(number))==lw:
+        if dic==Counter(discount[i:i+10]):
             cnt+=1
     return cnt
