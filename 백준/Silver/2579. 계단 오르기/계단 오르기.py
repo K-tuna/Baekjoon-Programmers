85217565fb485abfc9f19206080830ece8d1c376
dongwonkwon <90139277+dongwonkwon@users.noreply.github.com>
2023-07-16 03:09:49 +0900

    [Silver III] Title: 계단 오르기, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,18 +1,16 @@
-N = int(input())
-# dp에 사용할 배열과 각 계단에 저장할 값을 선언
-dp = [0] * (N+1)
-point = [0] * (N+1)
-for i in range(1, N+1):
-    point[i] = int(input())
-if N==1:
-    print(point[1])
-    exit()
-elif N==2:
-    print(sum(point[:3]))
-    exit()
-dp[1] = point[1]
-dp[2] = point[1]+point[2]
-for i in range(3, N+1):
-    dp[i] = max(dp[i-2]+point[i], dp[i-3]+point[i-1]+point[i])
+N=int(input())
+dp=[0]*(N+2)
+stairs=[0]*(N+1)
+for i in range(1,N+1):
+    stairs[i]=int(input())
 
-print(dp[-1])
+if N==1:
+    print(stairs[1])
+elif N==2:
+    print(stairs[1]+stairs[2])
+else:
+    dp[1] = stairs[1]
+    dp[2] = stairs[1] + stairs[2]
+    for i in range(3,N+1):
+        dp[i]=max(dp[i-2]+stairs[i], dp[i-3]+stairs[i-1]+stairs[i])
+    print(dp[N])
