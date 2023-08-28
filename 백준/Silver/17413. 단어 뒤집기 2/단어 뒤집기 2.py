45280915130c25fa4de9db740c100eac0a902c54
dongwonkwon <90139277+dongwonkwon@users.noreply.github.com>
2023-08-28 23:27:54 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
@@ -0,0 +1,12 @@
+text=input().replace('<','X<').replace('>','>X')
+tag_str = [t for t in text.split('X')]
+results=[]
+for ts in tag_str:
+    if '<' in ts and '>' in ts:
+        results.append(ts)
+    else:
+        words=ts.split()
+        reversed_words=[word[::-1]for word in words]
+        results.append(' '.join(reversed_words))
+
+print(''.join(results))
