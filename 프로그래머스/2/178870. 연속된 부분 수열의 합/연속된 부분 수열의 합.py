472421c64a6ea5bfee1ecf76563046bc3bddbcb0
dongwonkwon <ehddnjs0710@gmail.com>
2023-10-21 21:51:32 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.py
@@ -0,0 +1,16 @@
+def solution(sequence, k):
+    result=[]
+    end=0
+    interval_sum=0
+    sequence_len=len(sequence)
+    
+    for start in range(sequence_len):
+        while interval_sum < k and end < sequence_len:
+            interval_sum += sequence[end]
+            end += 1
+        if interval_sum == k:
+            result.append([start,end-1])
+            
+        interval_sum -= sequence[start]
+    result.sort(key=lambda x:(x[1]-x[0],x[0]))
+    return result[0]
