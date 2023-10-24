cc4419e09c6a214c342f647b07b21d7bdb8b0258
dongwonkwon <ehddnjs0710@gmail.com>
2023-10-24 19:13:02 +0900

    [level 2] Title: 호텔 대실, Time: 7.50 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
@@ -1,19 +1,13 @@
-import heapq
 def solution(book_time):
-    heap=[]
-    for time in book_time:
-        tmp1=time[0].split(':')
-        tmp2=time[1].split(':')
-        start=int(tmp1[0])*60+int(tmp1[1])
-        end=int(tmp2[0])*60+int(tmp2[1])+10
-        heapq.heappush(heap,(start,end))
-    
-    last=[0]
-    while heap:
-        now_in,now_out=heapq.heappop(heap)
-        if now_in>=last[0]:
-            heapq.heappop(last)
-        heapq.heappush(last,now_out)
+    time_table = [0 for _ in range(60 * 24)]
+    for start, end in book_time:
+        start_minutes = 60 * int(start[:2]) + int(start[3:])
+        end_minutes = 60 * int(end[:2]) + int(end[3:]) + 10
 
-    return len(last)
+        if end_minutes > 60 * 24 - 1:
+            end_minutes = 60 * 24 - 1
+
+        for i in range(start_minutes, end_minutes):
+            time_table[i] += 1
+    return max(time_table)
         
