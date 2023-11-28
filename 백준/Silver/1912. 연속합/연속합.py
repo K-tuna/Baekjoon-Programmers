44524ece787153fcebca10e40909b22a3f958c6b
dongwonkwon <ehddnjs0710@gmail.com>
2023-11-28 21:53:40 +0900

    [Silver II] Title: 연속합, Time: 100 ms, Memory: 38828 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -0,0 +1,7 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+numbers = list(map(int,input().split()))
+for i in range(1,N):
+    numbers[i] = max(numbers[i], numbers[i-1]+numbers[i])
+print(max(numbers))
