50871ea4855adc0d86414a560a4bc56c4a9a5b4d
dongwonkwon <ehddnjs0710@gmail.com>
2023-12-20 23:49:29 +0900

    [Silver III] Title: 수열, Time: 144 ms, Memory: 121160 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -1,9 +1,10 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
 n, k = map(int, input().split())
-arr = [*map(int, input().split())]
-
-cur = sum(arr[:k])
-answer = cur
-for i in range(k, n):
-    cur = cur + arr[i] - arr[i-k]
-    answer = max(cur, answer)
+arr = list(map(int,input().split()))
+answer = sum(arr[:k])
+tmp = answer
+for i in range(k,n):
+    tmp = tmp - arr[i - k] + arr[i]
+    answer = max(tmp, answer)
 print(answer)
