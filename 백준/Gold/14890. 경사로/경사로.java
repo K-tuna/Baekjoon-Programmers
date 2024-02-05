13b5843dc91929e8c06542ec35f1badfc67805ba
dongwonkwon <ehddnjs0710@gmail.com>
2024-02-05 13:35:46 +0900

    [Gold III] Title: 경사로, Time: 152 ms, Memory: 15212 KB -BaekjoonHub

---
+++ 백준/Gold/14890. 경사로/경사로.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int N;
+	static int L;
+	static int[][] map;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		// N,L 입력
+		N = Integer.parseInt(st.nextToken());
+		L = Integer.parseInt(st.nextToken());
+		map = new int[N][N];
+		
+		//배열 입력
+		for(int i=0; i<N; i++) {
+			st = new StringTokenizer(br.readLine()," ");
+			for(int j=0; j<N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		int cnt = 0;
+        for(int i=0; i<N; i++) {
+            if(isGo(i, 0, true)) cnt++;
+            if(isGo(0, i, false)) cnt++;
+        }
+        sb.append(cnt);
+        System.out.println(sb);
+	}
+	
+	public static boolean isGo(int x, int y, boolean isRow) {
+		int[] height = new int[N];
+		boolean[] isRunway = new boolean[N];
+		
+		for(int i=0; i<N; i++) {
+			if(isRow) height[i] = map[x][y+i];
+			else height[i] = map[x+i][y];
+		}
+		
+		for(int i=0; i<N-1; i++) {
+			int heightDiff = height[i] - height[i+1];
+			//높이차이 2이상이면 경사로 x
+			if(Math.abs(heightDiff)>=2) return false;
+			//높이차이 0이면 그냥 지나갈 수 있음
+			if(heightDiff == 0) continue;
+			//내리막이면
+			if(heightDiff == 1) {
+				for(int j=i+1; j<=i+L; j++) {
+					if(!isIn(j) || height[i+1] != height[j] || isRunway[j]) return false;
+					isRunway[j] = true;
+				}
+				i+=L-1;
+			}else if(heightDiff == -1) {
+				for(int j=i; j>i-L; j--) {
+					if(!isIn(j) || height[i] != height[j] || isRunway[j]) return false;
+					isRunway[j] = true;
+				}
+			}
+		}
+		
+		return true;
+	}
+	
+	public static boolean isIn(int x) {
+		return 0<=x && x<N;
+	}
+}
