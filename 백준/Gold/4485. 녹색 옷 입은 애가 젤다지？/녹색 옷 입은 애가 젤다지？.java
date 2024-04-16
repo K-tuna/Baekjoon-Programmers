31b16d152f183dafe60166ee2be3de6304db28b2
dongwonkwon <ehddnjs0710@gmail.com>
2024-04-16 16:36:55 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 1460 ms, Memory: 167332 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,82 +1,75 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-
+	
+	static class Node implements Comparable<Node>{
+		int r,c,w;
+		
+		public Node(int r, int c, int w) {
+			this.r = r;
+			this.c = c;
+			this.w = w;
+		}
+		
+		@Override
+		public int compareTo(Node o) {
+			return this.w - o.w;
+		}
+		
+	}
 	static int N;
 	static int[][] g;
-	static boolean[] v;
 	static int[][] dist;
-	static int[] dr = {1, -1, 0, 0};
-    static int[] dc = {0, 0, 1, -1};
-	
-	static StringBuilder sb = new StringBuilder();
+	static int[] dr = {0,0,-1,1};
+	static int[] dc = {-1,1,0,0};
 
+	static StringBuilder sb = new StringBuilder();
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int cnt=0;
 		while(true) {
-			N=Integer.parseInt(br.readLine());
+			N = Integer.parseInt(br.readLine());
 			if(N==0) break;
+			
 			g=new int[N][N];
-			v=new boolean[N];
 			dist=new int[N][N];
 			cnt++;
-			
-			
 			for(int i=0; i<N; i++) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
 				for(int j=0; j<N; j++) {
 					g[i][j]=Integer.parseInt(st.nextToken());
-					dist[i][j] = -1;
+					dist[i][j]=-1;
 				}
 			}
-			dijkstra(cnt);
+			dijstra(cnt);
 		}
 		System.out.println(sb);
+		
 	}
-	
-	static void dijkstra(int cnt){
-		PriorityQueue<Node> pq = new PriorityQueue<>();
+	static void dijstra(int cnt) {
+		PriorityQueue<Node> pq=new PriorityQueue<>();
+		dist[0][0]=g[0][0];
 		pq.offer(new Node(0,0,g[0][0]));
-		dist[0][0] = g[0][0];
 		
 		while(!pq.isEmpty()) {
-			Node a = pq.poll();
-			if(a.row == N-1 && a.col == N-1) {
-				sb.append("Problem ").append(cnt).append(": ").append(a.loopy).append("\n");
+			Node node=pq.poll();
+			if(node.r==N-1 && node.c==N-1) {
+				sb.append("Problem ").append(cnt).append(": ").append(node.w).append("\n");
 				break;
 			}
 			
-			for(int i=0; i<4; i++) {
-				int nr = a.row + dr[i];
-				int nc = a.col + dc[i];
+			for(int d=0; d<4; d++) {
+				int nr = node.r + dr[d];
+				int nc = node.c + dc[d];
 				
-				if(nr<0||nc<0||nr>=N||nc>=N)
+				if(nr<0 || nr>=N || nc<0 || nc>=N)
 					continue;
-				if(dist[nr][nc] >= a.loopy + g[nr][nc])
-					continue;
-				
-				dist[nr][nc] = a.loopy + g[nr][nc];
-				pq.offer(new Node(nr,nc,dist[nr][nc]));
+				if( dist[nr][nc] < node.w + g[nr][nc]) {
+					dist[nr][nc] = node.w + g[nr][nc];
+					pq.offer(new Node(nr,nc,dist[nr][nc]));
+				}
 			}
 		}
-		
 	}
-	
-}
-class Node implements Comparable<Node>{
-	int row, col, loopy;
-	
-	Node(int row, int col, int loopy){
-		this.row = row;
-		this.col = col;
-		this.loopy = loopy;
-	}
-	
-	@Override
-	public int compareTo(Node o) {
-		return this.loopy - o.loopy;
-	}
-	
 }
