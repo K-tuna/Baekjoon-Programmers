c8ebf2528d34c4689cc5023f34e1a98bcf61ab6a
dongwonkwon <ehddnjs0710@gmail.com>
2024-04-29 14:16:23 +0900

    [level 2] Title: H-Index, Time: 0.66 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -6,7 +6,6 @@ class Solution {
         List<Integer> list = new ArrayList<>();
         int ans = 0;
         for(int citation: citations) list.add(citation);
-        // Collections.sort(list, Collections.reverseOrder());
         Collections.sort(list, (o1,o2)-> o2-o1);
         System.out.print(list);
         for(int i=0; i<list.size(); i++){
