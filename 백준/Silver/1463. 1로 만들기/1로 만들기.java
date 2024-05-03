6c735a0b304888c5bb16239a5b5c65811d5aebd8
dongwonkwon <ehddnjs0710@gmail.com>
2024-05-03 17:45:14 +0900

    [Silver III] Title: 1로 만들기, Time: 224 ms, Memory: 127288 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,21 +1,36 @@
 import java.util.*;
 import java.io.*;
+
 public class Main {
-	static StringBuilder sb = new StringBuilder();
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int max = (int)Math.pow(10, 6)+1;
-		int[] dp = 	new int[max];
-		
-		for(int i=2; i<N+1; i++) {
-			dp[i]=dp[i-1]+1;
-			if(i%2==0)
-				dp[i]=Math.min(dp[i], dp[i/2]+1);
-			if(i%3==0)
-				dp[i]=Math.min(dp[i], dp[i/3]+1);
-		}
-		System.out.println(dp[N]);
-	}
+	static int N;
+	static Queue<int[]> q=new ArrayDeque<>();
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N=Integer.parseInt(br.readLine());
+        
+        int answer=0;
+        int[] arr=new int[]{N,0};
+        int[] sub;
+        int a;
+        q.offer(arr);
+        while(!q.isEmpty()) {
+        	sub=q.poll();
+        	a=sub[0];
+        	//System.out.println(a);
+        	if(a==1) {
+        		answer=sub[1];
+        		break;
+        	}
+        	if(a%3==0) {
+        		q.offer(new int[] {a/3,sub[1]+1});
+        	}
+        	if(a%2==0) {
+        		q.offer(new int[] {a/2,sub[1]+1});
+        	} 
+        	q.offer(new int[] {a-1,sub[1]+1});
+        }
+        
+        System.out.println(answer);
+        
+    }
 }
