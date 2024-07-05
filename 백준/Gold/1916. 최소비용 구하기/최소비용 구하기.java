64828d56b40990d2fd42e548682b8f4900bc0ff2
dongwonkwon <ehddnjs0710@gmail.com>
2024-07-05 15:21:56 +0900

    [Gold V] Title: 최소비용 구하기, Time: 476 ms, Memory: 51256 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,71 +1,71 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-	static class Node{
-		int vertex,weight;
-		Node next;
-		
-		public Node(int vertex, int weight, Node next) {
-			this.vertex = vertex;
-			this.weight = weight;
-			this.next = next;
+	static class Node implements Comparable<Node>{
+		int end,w;
+		public Node(int end,int w) {
+			this.end=end;
+			this.w=w;
 		}
 		@Override
-		public String toString() {
-			return "Node [vertex=" + vertex + ", weight=" + weight + ", next=" + next + "]";
-		}		
+		public int compareTo(Node o) {
+			return this.w-o.w;
+		}
 	}
+	static int V,E,A,B;
+	static final int INF = 100_000_000;
+	
+	static boolean[] v;
+	static int[] dist;
+	static List<Node>[] list;
 	
 	static StringBuilder sb = new StringBuilder();
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int V = Integer.parseInt(st.nextToken());
-		st = new StringTokenizer(br.readLine());
-		int E = Integer.parseInt(st.nextToken());
-		final int INF = Integer.MAX_VALUE;
 		
-		Node[] adjList = new Node[V+1];
-		int[] minDistance = new int[V+1];
-		boolean[] visited = new boolean[V+1];
+		V = Integer.parseInt(br.readLine());
+		E = Integer.parseInt(br.readLine());
 		
-		for(int i=0; i<E; i++) {
-			st = new StringTokenizer(br.readLine());
-			int from = Integer.parseInt(st.nextToken());
-			int toto = Integer.parseInt(st.nextToken());
-			int weight = Integer.parseInt(st.nextToken());
-			adjList[from] = new Node(toto, weight, adjList[from]);
-		}
-		st = new StringTokenizer(br.readLine());
-		int start = Integer.parseInt(st.nextToken());
-		int end = Integer.parseInt(st.nextToken());
-		
-		
-		Arrays.fill(minDistance, INF);
-		minDistance[start] = 0;
-		
-		int min=0, stopOver=0;
+		v = new boolean[V+1];
+		dist = new int[V+1];
+		list = new List[V+1];
 		for(int i=1; i<V+1; i++) {
-			min = INF;
-			stopOver = -1;
+			dist[i] = INF;
+			list[i] = new ArrayList<>();
+		}
+		for(int i=0; i<E; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			int weight = Integer.parseInt(st.nextToken());
+			list[start].add(new Node(end,weight));
+		}
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		A = Integer.parseInt(st.nextToken());
+		B = Integer.parseInt(st.nextToken());
+		dijkstra();
+		System.out.println(dist[B]);
+	}
+	
+	static void dijkstra() {
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		dist[A] = 0;
+		pq.add(new Node(A,0));
+		
+		while(!pq.isEmpty()) {
+			Node curNode = pq.poll();
+			int end = curNode.end;
 			
-			for(int j=1; j<V+1; j++) {
-				if(!visited[j] && min>minDistance[j]) {
-					min = minDistance[j];
-					stopOver = j;
-				}
-			}
+			if(v[end]) continue;
+			v[end] = true;
 			
-			if(stopOver==-1) break;
-			visited[stopOver]=true;
-			
-			for(Node temp=adjList[stopOver]; temp!=null; temp=temp.next) {
-				if(minDistance[temp.vertex] > min+temp.weight) {
-					minDistance[temp.vertex] = min+temp.weight;
+			for(Node node : list[end]) {
+				if( dist[node.end] > dist[end] + node.w) {
+					dist[node.end] = dist[end] + node.w;
+					pq.add(new Node(node.end, dist[node.end]));
 				}
 			}
 		}
-		System.out.println(minDistance[end]);
 	}
 }
