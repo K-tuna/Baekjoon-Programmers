2d72020f1b6ad48f1dbb68cd04583ce2f6d10752
K-tuna <ehddnjs0710@gmail.com>
2025-04-06 22:10:20 +0900

    [level 1] Title: 예산, Time: 1.05 ms, Memory: 91.8 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -1,20 +1,17 @@
 import java.util.*;
 class Solution {
     public int solution(int[] d, int budget) {
-        int cnt = 0;
-        int sum = 0;
         List<Integer> list = new ArrayList<>();
         for(int n : d) {
             list.add(n);
         }
         list.sort((a,b) -> a-b);
+        int cnt = 0;
+        int sum = 0;
         for(int n : list) {
-            if(sum + n <= budget){
-                sum += n;
-                cnt ++;
-            }else {
-                return cnt;
-            }
+            if(sum + n > budget) break;
+            sum += n;
+            cnt ++;
         }
         return cnt;
     }
