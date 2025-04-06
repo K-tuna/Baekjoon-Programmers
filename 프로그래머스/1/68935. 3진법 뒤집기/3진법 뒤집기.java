3f04ba6529d8374dc97adb76e3b012eff66e5811
K-tuna <ehddnjs0710@gmail.com>
2025-04-06 23:14:09 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.08 ms, Memory: 88.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,7 @@
+import java.util.*;
+import java.io.*;
+class Solution {
+    public int solution(int n) {
+        return Integer.parseInt(new StringBuilder(Integer.toString(n, 3)).reverse().toString(), 3);
+    }
+}
