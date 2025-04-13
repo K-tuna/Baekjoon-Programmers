23f28e2e811640f5c1db9e22df88fba46870f64f
K-tuna <ehddnjs0710@gmail.com>
2025-04-14 05:57:05 +0900

    [level 1] Title: [1차] 비밀지도, Time: 7.44 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,19 +1,13 @@
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] map = new String[n];
-        String[] str1 = new String[n];
-        String[] str2 = new String[n];
         for(int i=0; i<n; i++){
-            str1[i] = String.format("%" + n + "s",Integer.toBinaryString(arr1[i])).replace(" ","0");
-            str2[i] = String.format("%" + n + "s",Integer.toBinaryString(arr2[i])).replace(" ","0");
+            map[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
         }
         for(int i=0; i<n; i++){
-            String str = "";
-            for(int j=0; j<n; j++){
-                if(str1[i].charAt(j)=='0' && str2[i].charAt(j)=='0') str += " ";
-                else str += "#";
-            }
-            map[i] = str;
+            map[i] = String.format("%" + n + "s", map[i]);
+            map[i] = map[i].replaceAll("1", "#");
+            map[i] = map[i].replaceAll("0", " ");
         }
         return map;
     }
