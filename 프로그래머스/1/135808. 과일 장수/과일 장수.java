ce02377f0b7c085cf7c8d00c373c45f3492c1265
K-tuna <ehddnjs0710@gmail.com>
2025-04-19 15:49:49 +0900

    [level 1] Title: 과일 장수, Time: 60.59 ms, Memory: 141 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -2,13 +2,9 @@ import java.util.*;
 class Solution {
     public int solution(int k, int m, int[] score) {
         int max = 0;
-        List<Integer> list = new ArrayList<>();
-        for(int n : score) {
-            list.add(n);
-        }
-        list.sort((a,b) -> b-a);
-        for(int i=m-1; i<score.length; i+=m){
-            max += list.get(i)*m;
+        Arrays.sort(score);
+        for(int i=score.length; i>=m; i-=m){
+            max += score[i-m]*m;
         }
         return max;
     }
